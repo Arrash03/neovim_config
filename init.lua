8fbcf09750c8ae3e991c97519398ab346deb5e3e
Arrash Tahery <107276674+Arrash03@users.noreply.github.com>
2024-08-22 20:28:31 +0330

    Add files via upload
    first theme initialization

---
+++ init.lua
@@ -0,0 +1,169 @@
+local vim = vim
+local Plug = vim.fn['plug#']
+
+vim.call('plug#begin')
+
+-- List your plugins here
+Plug('tpope/vim-sensible')
+
+-- vim-airline
+Plug('vim-airline/vim-airline')
+Plug('vim-airline/vim-airline-themes')
+
+-- multi
+Plug('mg979/vim-visual-multi')
+
+-- Using Vim-Plug
+Plug('navarasu/onedark.nvim')
+
+-- clangd
+Plug('p00f/clangd_extensions.nvim')
+
+
+vim.call('plug#end')
+
+-- Lua
+require('onedark').setup  {
+    -- Main options --
+    style = 'darker', -- Default theme style. Choose between 'dark', 'darker', 'cool', 'deep', 'warm', 'warmer' and 'light'
+    transparent = false,  -- Show/hide background
+    term_colors = true, -- Change terminal color as per the selected theme style
+    ending_tildes = true, -- Show the end-of-buffer tildes. By default they are hidden
+    cmp_itemkind_reverse = true, -- reverse item kind highlights in cmp menu
+
+    -- toggle theme style ---
+    toggle_style_key = nil, -- keybind to toggle theme style. Leave it nil to disable it, or set it to a string, for example "<leader>ts"
+    toggle_style_list = {'dark', 'darker', 'cool', 'deep', 'warm', 'warmer', 'light'}, -- List of styles to toggle between
+
+    -- Change code style ---
+    -- Options are italic, bold, underline, none
+    -- You can configure multiple style with comma separated, For e.g., keywords = 'italic,bold'
+    code_style = {
+        comments = 'italic',
+        keywords = 'italic',
+        functions = 'italic',
+        strings = 'none',
+        variables = 'none'
+    },
+
+    -- Lualine options --
+    lualine = {
+        transparent = false, -- lualine center bar transparency
+    },
+
+    -- Custom Highlights --
+    colors = {
+	bright_orange = "#ff8800",    -- define a new color
+    	green = '#80C060',            -- redefine an existing color,
+	blue = '#27A7F3',
+	purple = '#CD5FDF',
+    },
+    highlights = {
+    	TSKeyword = {fg = '$green'},
+    	TSString = {fg = '$bright_orange', fmt = 'bold'},
+    	TSFunction = {fg = '$blue', sp = '$cyan', fmt = 'underline,italic'},
+    	TSFuncBuiltin = {fg = '$purple'}
+    },
+    -- Plugins Config --
+    diagnostics = {
+        darker = true, -- darker colors for diagnostic
+        undercurl = true,   -- use undercurl instead of underline for diagnostics
+        background = true,    -- use background color for virtual text
+    },
+}
+require('onedark').load()
+
+-- clangd
+require("clangd_extensions").setup({
+    inlay_hints = {
+        inline = vim.fn.has("nvim-0.10") == 1,
+        -- Options other than `highlight' and `priority' only work
+        -- if `inline' is disabled
+        -- Only show inlay hints for the current line
+        only_current_line = false,
+        -- Event which triggers a refresh of the inlay hints.
+        -- You can make this { "CursorMoved" } or { "CursorMoved,CursorMovedI" } but
+        -- note that this may cause higher CPU usage.
+        -- This option is only respected when only_current_line is true.
+        only_current_line_autocmd = { "CursorHold" },
+        -- whether to show parameter hints with the inlay hints or notdeb
+        show_parameter_hints = true,
+        -- prefix for parameter hints
+        parameter_hints_prefix = "<- ",
+        -- prefix for all the other hints (type, chaining)
+        other_hints_prefix = "=> ",
+        -- whether to align to the length of the longest line in the file
+        max_len_align = false,
+        -- padding from the left if max_len_align is true
+        max_len_align_padding = 1,
+        -- whether to align to the extreme right or not
+        right_align = false,
+        -- padding from the right if right_align is true
+        right_align_padding = 7,
+        -- The color of the hints
+        highlight = "Comment",
+        -- The highlight group priority for extmark
+        priority = 100,
+    },
+    ast = {
+        -- These are unicode, should be available in any font
+        role_icons = {
+            type = "🄣",
+            declaration = "🄓",
+            expression = "🄔",
+            statement = ";",
+            specifier = "🄢",
+            ["template argument"] = "🆃",
+        },
+        kind_icons = {
+            Compound = "🄲",
+            Recovery = "🅁",
+            TranslationUnit = "🅄",
+            PackExpansion = "🄿",
+            TemplateTypeParm = "🅃",
+            TemplateTemplateParm = "🅃",
+            TemplateParamObject = "🅃",
+        },
+        --[[ These require codicons (https://github.com/microsoft/vscode-codicons)
+            role_icons = {
+                type = "",
+                declaration = "",
+                expression = "",
+                specifier = "",
+                statement = "",
+                ["template argument"] = "",
+            },
+
+            kind_icons = {
+                Compound = "",
+                Recovery = "",
+                TranslationUnit = "",
+                PackExpansion = "",
+                TemplateTypeParm = "",
+                TemplateTemplateParm = "",
+                TemplateParamObject = "",
+            }, ]]
+
+        highlights = {
+            detail = "Comment",
+        },
+    },
+    memory_usage = {
+        border = "none",
+    },
+    symbol_info = {
+        border = "none",
+    },
+})
+
+-- vim-airline
+vim.cmd("let g:airline#extensions#tabline#enabled = 1")
+vim.cmd("let g:airline#extensions#tabline#left_sep = ' '")
+vim.cmd("let g:airline#extensions#tabline#left_sep = '|'")
+vim.cmd("let g:airline#extensions#tabline#formatter = 'default'")
+
+-- multi
+vim.cmd("let g:airline_theme='luna'")
+
+
+
